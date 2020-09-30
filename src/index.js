feaceeb011b5c9f4a983bedaa67946672859d377
rasolko <vano2029492@mail.ru>
2020-09-30 18:29:45 +0300

    feat: add functions: max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length === 0 || array == 0) {
+    return 0
+  }
+  let minNumber = array[0];
+  array.map( item => {
+    if(item < minNumber) minNumber = item
+  })
+  return minNumber
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length === 0 || array == 0) {
+    return 0
+  }
+  else{
+    let maxNumber = array[0];
+  array.map( item => {
+    if(item > maxNumber) maxNumber = item
+  })
+  return maxNumber
+  }
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length === 0 || array == 0) {
+    return 0
+  }
+  let sum = 0;
+  array.map( item => {
+    sum += item;
+  })
+  return sum / array.length;
 }
